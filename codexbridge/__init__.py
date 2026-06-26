c689a978a3d26eece316912b815236c46aeb39f8
Arashmehrdad <54375392+Arashmehrdad@users.noreply.github.com>
2026-06-26 04:21:22 +0100

    feat: install repository knowledge tools

--- codexbridge/__init__.py
+++ codexbridge/__init__.py
@@ -1,8 +1,10 @@
 """CodexBridge local MCP bridge."""
 
+from .knowledge_tools_integration import install_knowledge_tools
 from .repo_discovery_integration import install_repo_discovery
 
 
 __version__ = "0.1.0"
 
 install_repo_discovery()
+install_knowledge_tools()
